fe3c3f17ef7367e492151514531932fbb4ee5124
Nikhil George <nikhgeo@gmail.com>
2014-09-15 14:26:16 +1000

    Update nexus.sh

--- nexus.sh
+++ nexus.sh
@@ -3,7 +3,7 @@ htmlcontent=`curl http://www.saholic.com/mobile-phones/lg-e960-nexus-4-16gb-1005
 if [[ $htmlcontent == *"ADD TO CART"* ]]
 then
 	echo "ADD TO CART" > /tmp/nexusemail.txt
-	/bin/mail -s "NEXUS AVAIALBAILITY : AVAILABLE" nikhgeo@gmail.com < /tmp/nexusemail.txt
+	/bin/mail -s "NEXUS AVAIALBAILITY : AVAILABLE" <youremail>@gmail.com < /tmp/nexusemail.txt
 else
 	echo "NOT AVAILABLE"
 fi
